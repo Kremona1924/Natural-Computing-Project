1a177174923f43663667b614b07374853b43c87a
Alexander de Ranitz <a.deranitz@roboteamtwente.nl>
2024-06-04 19:20:16 +0200

    more plots

--- visualize_performance.py
+++ visualize_performance.py
@@ -1,36 +1,125 @@
 import glob
 import json
 import pandas as pd
+import os
+import numpy as np
 import matplotlib.pyplot as plt
+import bisect
+import itertools
+
+mutation_rate_experiments = [0.01, 0.1, 0.3, 0.5]
+mutation_step_experiments = [0.01, 0.1, 0.2, 0.4]
+tournament_size_experiments = [1, 2, 4, 10]
+crossover_type_experiments = ['none', 'uniform', 'single_point', 'mean']
+
+def get_name(cot, mr, ms, sel):
+    return f"performance_steps500_pop25_gens50_cot={cot}_mr{mr:.0e}_ms{ms:.0e}_{sel}.json"
+
+def parse_name(name):
+    mr = name.split("mr")[1].split("_")[0]
+    ms = name.split("ms")[1].split("_")[0]
+    cot = name.split("cot=")[1].split("_mr")[0]
+    sel = name.split("ts")[-1].split(".")[0]
+    return cot, float(mr), float(ms), int(sel)
+
+run_folder = "logs/"
+
+# data = {key : np.empty((64*5,51)) for key in tournament_size_experiments}
+# counts = {key : 0 for key in tournament_size_experiments}
+
+# for f1 in os.listdir(run_folder):
+#     for f2 in os.listdir(run_folder + f1):
+#         for f3 in os.listdir(run_folder + f1 + '/' + f2):
+#             if f3[:11] == "performance":
+#                 with open(run_folder + f1 + '/' + f2 + '/' + f3, 'r') as f:
+#                     d = json.load(f)
+#                     cot, mr, ms, sel = parse_name(f3)
+#                     data[sel][counts[sel]] = d['mean_fitness_over_time']
+#                     counts[sel] += 1
+
+# for k, v in data.items():
+#     plt.plot(np.mean(v, axis=0), label="Tournament size = " + str(k))
+
+# plt.legend()
+# plt.show()
+
+# #======================================================
+# N = 10
+# best = [100] * N
+# names = [None] * N
+
+# for f1 in os.listdir(run_folder):
+#     for f2 in os.listdir(run_folder + f1):
+#         for f3 in os.listdir(run_folder + f1 + '/' + f2):
+#             if f3[:11] == "performance":
+#                 with open(run_folder + f1 + '/' + f2 + '/' + f3, 'r') as f:
+#                     d = json.load(f)['mean_fitness_over_time']
+#                     mean = np.mean(d[-10:])
+#                     if mean < best[-1]:
+#                         best += [mean]
+#                         names += [f3]
+#                         ind = np.argsort(best)
+#                         best = [best[i] for i in ind][:-1]
+#                         names = [names[i] for i in ind][:-1]
+
+# print(best)
+# [print(parse_name(n)) for n in names]
 
 
-#path to run folder
-run_folder = r"logs\experiment03\run03"
+#=====================================================================
 
-file_path = run_folder + "/performance_*"
-match_path = glob.glob(file_path)
-with open(match_path[0], 'r') as f:
-    data = json.load(f)
+# mutation_step_experiments = mutation_step_experiments[::-1]
+# data = {(mr, ms) : 0.0 for mr, ms in itertools.product(mutation_rate_experiments, mutation_step_experiments)}
 
-keys = list(data.keys())
-pd_data = {key: data[key] for key in keys[:-4]}
+# print(data.keys())
+# for f1 in os.listdir(run_folder):
+#     for f2 in os.listdir(run_folder + f1):
+#         for f3 in os.listdir(run_folder + f1 + '/' + f2):
+#             if f3[:11] == "performance":
+#                 with open(run_folder + f1 + '/' + f2 + '/' + f3, 'r') as f:
+#                     d = json.load(f)['mean_fitness_over_time']
+#                     cot, mr, ms, sel = parse_name(f3)
+#                     if cot == "single_point":
+#                         mean = np.mean(d[-10:])
+#                         data[(mr, ms)] += mean
 
-df = pd.DataFrame.from_dict(pd_data, orient='index', columns=[''])
-print(df)
+# data_2d = np.zeros((len(mutation_rate_experiments), len(mutation_step_experiments)))
+# for i, mr in enumerate(mutation_rate_experiments):
+#     for j, ms in enumerate(mutation_step_experiments):
+#         data_2d[i,j] = data[(mr, ms)]
 
-graph_data = {key: data[key] for key in keys[-4:]}
+# data_2d /= 4 * 5
 
-# Plot each data series
-for key, values in graph_data.items():
-    plt.plot(values, label=key)
+# plt.imshow(data_2d, cmap='Blues', interpolation='nearest')
+# plt.title("Effects of mutation rate and size\n(crossover type = uniform)")
+# plt.xticks(range(len(mutation_rate_experiments)), mutation_rate_experiments)
+# plt.yticks(range(len(mutation_step_experiments)), mutation_step_experiments)
+# plt.xlabel("Mutation rate")
+# plt.ylabel("Mutation step size")
+# plt.colorbar(label="Mean fitness over last 10 generations")
 
-# Add legend
-plt.legend()
+# for (j,i),label in np.ndenumerate(data_2d):
+#     plt.text(i,j,f"{label:.3f}",ha='center',va='center')
 
-# Add labels and title
-plt.xlabel('Time')
-plt.ylabel('Value')
-plt.title('Values Over Time')
+# plt.show()
 
-# Show plot
-plt.show()
+
+#=====================================================================
+data = {(mr, ms, cot, sel) : 0.0 for mr, ms, cot, sel in itertools.product(mutation_rate_experiments, mutation_step_experiments, crossover_type_experiments, tournament_size_experiments)}
+
+for f1 in os.listdir(run_folder):
+    for f2 in os.listdir(run_folder + f1):
+        for f3 in os.listdir(run_folder + f1 + '/' + f2):
+            if f3[:11] == "performance":
+                with open(run_folder + f1 + '/' + f2 + '/' + f3, 'r') as f:
+                    d = json.load(f)['mean_fitness_over_time']
+                    cot, mr, ms, sel = parse_name(f3)
+                    mean = np.mean(d[-10:])
+                    data[(mr, ms, cot, sel)] += mean
+
+for k in data.keys():
+    data[k] /= 5
+
+best_inds = np.argsort(list(data.values()))[:10]
+for i in best_inds:
+    print("Parameters:", list(data.keys())[i])
